021f1e8257db1a746b7fea8ec68070c8ea6b7667
Brendan <brendanmoratz@hotmail.com>
2020-08-09 20:49:18 +0200

    Style H1

---
+++ src/components/H1/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components/macro'
+
+export const H1 = styled.h1`
+  font-size: 9rem;
+  font-weight: 300;
+  width: 8ch;
+  color: #fff;
+  text-shadow: -3px -3px 0 #000, 3px -3px 0 #000, -3px 3px 0 #000,
+    3px 3px 0 #000;
+  margin: 0;
+`
