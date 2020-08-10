796f555f8e39ab95af4d4265210ddb30928bb4e5
Brendan <brendanmoratz@hotmail.com>
2020-08-10 18:51:40 +0200

    Add styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,51 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import { H1 } from './H1'
+import styled from 'styled-components/macro'
+import StyledBackgroundSection from './BgImage'
+import { SiteTitle } from './icons/SiteTitle'
+import '../styles/text-animation.css'
+
+// const BorderWrapper = styled.div`
+//   position: absolute;
+//   top: 0;
+//   left: 0;
+//   right: 0;
+//   bottom: 0;
+//   box-shadow: 0 0 0 6rem inset;
+//   z-index: -1;
+// `
+const TitleWrapper = styled.div`
+  width: 100vw;
+  position: absolute;
+  top: 10rem;
+  left: 5vw;
+  & > svg {
+    max-width: 55vw;
+  }
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  position: relative;
+`
+const StyledHeader = styled.header`
+  position: relative;
+`
 
 const Header = ({ siteTitle }) => (
-  <header>
-    <div>
-      <H1>{siteTitle}</H1>
-    </div>
-  </header>
+  <StyledHeader>
+    <Wrapper>
+      <StyledBackgroundSection siteTitle={siteTitle}>
+        {/* <BorderWrapper /> */}
+      </StyledBackgroundSection>
+    </Wrapper>
+    <TitleWrapper className='puff-in-left'>
+      <SiteTitle />
+    </TitleWrapper>
+  </StyledHeader>
 )
 
 Header.propTypes = {
