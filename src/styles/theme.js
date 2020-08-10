5d70f1b33cb0cdc710067e278f672e92da8db38b
Brendan <brendanmoratz@hotmail.com>
2020-08-10 18:47:11 +0200

    Add theme

---
+++ src/styles/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+  colors: {
+    textPrimary: '#E0E0E0',
+    textSecondary: '#CACACA',
+  },
+  breakpoints: {
+    // leave this as a number to allow it to interact with logic
+    mobile: 970,
+  },
+}
