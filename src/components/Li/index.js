02b9821865803f71f573e37a51ef6cf5baf4f709
Brendan <brendanmoratz@hotmail.com>
2020-08-13 14:08:43 +0200

    Add styles

---
+++ src/components/Li/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components/macro'
+import { theme } from '../../styles/theme'
+
+export const Li = styled.li`
+  color: ${theme.colors.textPrimary};
+  font-weight: 200;
+  font-size: 1.1875rem;
+  line-height: 2em;
+  letter-spacing: 0.025em;
+  list-style-type: '-  ';
+  list-style-position: inside;
+`
