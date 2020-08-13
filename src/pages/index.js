2a3e030a29bfd5aaa21b7d1feb065f12ce15a726
Brendan <brendanmoratz@hotmail.com>
2020-08-13 09:27:26 +0200

    Add basic layout with styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,41 +6,72 @@ import { P } from '../components/P'
 import { H2 } from '../components/H2'
 import { H1 } from '../components/H1'
 import { HR } from '../components/HR'
-import styled from 'styled-components'
+import styled from 'styled-components/macro'
 
 const BodyWrapper = styled.div`
   height: 100vh;
+`
+const Row = styled.div`
+  display: flex;
+  margin: 4rem 8.125rem;
+
+  /* max-width: 73.75rem; */
+
+  div:nth-child(2) {
+    margin-left: 5rem;
+  }
+`
+const Column = styled.div`
+  width: 100%;
+  max-width: 33rem;
 `
 
 const IndexPage = () => (
   <Layout>
     <SEO title='Home' />
     <BodyWrapper>
-      <H1>Brendan Moratz</H1>
-      <HR />
-      <H2>Biography</H2>
-      <P>
-        Et ultrices neque ornare aenean euismod. Enim facilisis gravida neque
-        convallis. Facilisi etiam dignissim diam quis enim lobortis scelerisque
-        fermentum. Facilisis gravida neque convallis a cras semper auctor neque
-        vitae. Sollicitudin aliquam ultrices sagittis orci a scelerisque. Luctus
-        accumsan tortor posuere ac. Vulputate dignissim suspendisse in est ante
-        in nibh mauris. Imperdiet dui accumsan sit amet nulla. Sagittis orci a
-        scelerisque purus semper eget duis.
-      </P>
-      <P>
-        A condimentum vitae sapien pellentesque habitant morbi tristique. Cras
-        tincidunt lobortis feugiat vivamus at augue. Mi ipsum faucibus vitae
-        aliquet nec ullamcorper sit. Et ultrices neque ornare aenean euismod.
-        Enim facilisis gravida neque convallis. Facilisi etiam dignissim diam
-        quis enim lobortis scelerisque fermentum. Facilisis gravida neque
-        convallis a cras semper auctor neque vitae. Sollicitudin aliquam
-        ultrices sagittis orci a scelerisque. Luctus accumsan tortor posuere ac.
-        Vulputate dignissim suspendisse in est ante in nibh mauris. Imperdiet
-        dui accumsan sit amet nulla. Sagittis orci a scelerisque purus semper
-        eget duis.
-      </P>
-      <HR />
+      <Row>
+        <H1>Brendan Moratz</H1>
+      </Row>
+      <Row>
+        <HR />
+      </Row>
+      <Row>
+        <Column>
+          <H2>Biography</H2>
+          <P>
+            I am a self-taught web developer from Canada and am very excited
+            about all the creative possibilities there are in modern web
+            development. I have been studying Front-end development through an
+            open-source project called The Odin Project and have also worked as
+            a PHP/JS Programmer at GoOut s.r.o..
+          </P>
+          <P>
+            I have a Bachelor’s Degree in Music Performance with a focus on
+            engineering and music production. Due to COVID my hours have been
+            severely reduced at GoOut s.r.o. and have been actively seeking a
+            Junior Front-end position with React. I have a lot to learn but I
+            have been taking lessons/pair programming with a senior developer
+            friend of mine for the past month as well as self-studying for ~15
+            hours/wk for the past 3 months.
+          </P>
+        </Column>
+        <Column>
+          <P>
+            My current preferred stack is: React, Redux + Redux Toolkit,
+            styled-components, react-router-dom, formik, eslint, prettier.
+          </P>
+          <P>
+            I am interested in continuing my learning in web development. As a
+            Junior developer I expect to spend a lot of time making mistakes and
+            having to recover fast, but this is a cycle I have become very used
+            to in both my self-study and my lifetime of musical practice.
+          </P>
+        </Column>
+      </Row>
+      <Row>
+        <HR />
+      </Row>
       {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
     	  <Image />
     	</div> */}
