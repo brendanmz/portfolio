e7ed3244471dab235a2182a49e25f82cde6ca1fe
Brendan <brendanmoratz@hotmail.com>
2020-08-14 16:10:46 +0200

    Add svg's

---
+++ src/components/icons/linkedin.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Linkedin = () => (
+  <svg viewBox='0 0 72 72' xmlns='http://www.w3.org/2000/svg'>
+    <g fill='none' fillRule='evenodd'>
+      <path
+        d='M8 72h56a8 8 0 008-8V8a8 8 0 00-8-8H8a8 8 0 00-8 8v56a8 8 0 008 8z'
+        fill='none'
+      />
+      <path
+        d='M62 62H51.3V43.8c0-5-1.9-7.8-5.8-7.8-4.3 0-6.6 3-6.6 7.8V62H28.6V27.3H39V32s3.1-5.7 10.5-5.7C56.7 26.3 62 30.8 62 40v22zM16.3 22.8a6.4 6.4 0 01-6.3-6.4c0-3.5 2.8-6.4 6.3-6.4 3.6 0 6.4 2.9 6.4 6.4 0 3.5-2.8 6.4-6.4 6.4zM11 62h10.8V27.3H11V62z'
+        fill='#CACACA'
+      />
+    </g>
+  </svg>
+)
