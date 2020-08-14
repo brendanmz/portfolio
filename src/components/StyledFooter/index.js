cc1128805d27b159d8eeeeeb53bc1e1520053ea6
Brendan <brendanmoratz@hotmail.com>
2020-08-14 16:10:29 +0200

    Rename for clarity

--- src/components/StyledFooter/index.js
+++ src/components/StyledFooter/index.js
@@ -1,8 +1,12 @@
 import styled from 'styled-components/macro'
 import { theme } from '../../styles/theme'
 
-export const Footer = styled.footer`
+export const StyledFooter = styled.footer`
   color: ${theme.colors.textSecondary};
   padding: 3rem 0 1rem;
   border-top: 1px solid #b0b0b0;
+
+  & > * {
+    padding-right: 0.7rem;
+  }
 `
